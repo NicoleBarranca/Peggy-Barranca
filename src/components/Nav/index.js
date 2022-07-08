367bb9ffa07ecc771d7060e2f8a6cf7965e0f9ad
Nicole Barranca <nikkibarranca@gmail.com>
2022-07-08 10:50:09 -0400

    image folder path working

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,9 +1,11 @@
 import React from "react";
+import maggie from "../../img/maggie.png";
 
 function Nav() {
   return (
     <nav>
       <h1>MaggieMaggee</h1>
+      <img src={maggie} alt={"logo"} />
       <ul>
         <li>About</li>
         <li>Gallery</li>

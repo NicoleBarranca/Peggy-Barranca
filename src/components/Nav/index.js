3158f224e2eaa60df6858fd6e4f9dbcf72c22360
Nicole Barranca <nikkibarranca@gmail.com>
2022-07-07 21:23:56 -0400

    nav image

---
+++ src/components/Nav/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import maggie-magee from "../../images/maggie-mageepng";
+
+function Nav() {
+  return (
+    <nav>
+      <img src="src/images/maggie-magee.png" alt="maggie magee logo"></img>
+    </nav>
+  );
+}
+
+export default Nav;

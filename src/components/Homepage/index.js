06b20503ac13a4e3637d5f6caace6b31aa631b68
Nicole Barranca <nikkibarranca@gmail.com>
2022-07-09 12:15:51 -0400

    hero image

--- src/components/Homepage/index.js
+++ src/components/Homepage/index.js
@@ -4,8 +4,8 @@ import placeholder from "../../img/placeholder.jpg"
 function Homepage(){
     return (
         <div id="parent">
-        <div id="hero">hgf</div>
-        <img></img>
+        <div id="hero"><img src={placeholder} alt={"abstract art"}  /></div>
+                
         <h1 className="heroTitle">Multimedia realism meats abstract</h1>
         <button className="btn">Veiw Gallery</button>
         </div>

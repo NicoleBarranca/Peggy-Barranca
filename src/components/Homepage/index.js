2b11d38696f4efe5325372ca90a22d483dc223da
Nicole Barranca <nikkibarranca@gmail.com>
2022-07-12 12:16:49 -0400

    homepage qupte and about preview with mobile responsiveness

--- src/components/Homepage/index.js
+++ src/components/Homepage/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import placeholder from "../../img/placeholder.jpg";
+import maggie from "../../img/maggie.png";
 
 function Homepage() {
   return (
@@ -12,6 +13,24 @@ function Homepage() {
           <h1 className="heroTitle">Multimedia realism meats abstract</h1>
           <button className="btn">Veiw Gallery</button>
         </div>
+      </div>
+      <div className="quote">
+        “I am always doing what I cannot do yet, in order to learn how to do
+        it.”
+        <br></br>
+        <span>― Vincent Van Gogh</span>
+      </div>
+      <div className="aboutPrev">
+        <div>
+          {" "}
+          <p>
+            Maggie Magee is an Artist from New Jersey whose passion has no
+            limits. Maggie has taught art is both public and private settings.
+          </p>
+          <button className="btn">Learn More</button>
+        </div>
+
+        <img src={maggie} alt={"logo"} id="logo" style={{ width: 200 }} />
       </div>
     </div>
   );

35e870a27b9dc257ed1c15e99782b44f60f3f26d
Nicole Barranca <nikkibarranca@gmail.com>
2022-07-11 21:40:16 -0400

    background image edits

--- src/components/Homepage/index.js
+++ src/components/Homepage/index.js
@@ -3,13 +3,21 @@ import placeholder from "../../img/placeholder.jpg";
 
 function Homepage() {
   return (
-    <div id="parent">
-      <div id="hero">
-        <img src={placeholder} alt={"abstract art"} />
-      </div>
+    <div>
+      <div id="parent">
+        <div id="hero">
+          <img className="heroImage" src={placeholder} alt={"abstract art"} />
+        </div>
 
-      <h1 className="heroTitle">Multimedia realism meats abstract</h1>
-      <button className="btn">Veiw Gallery</button>
+        <h1 className="heroTitle">Multimedia realism meats abstract</h1>
+        <button className="btn">Veiw Gallery</button>
+      </div>
+      <div>
+        <h2>
+          “Normality is a paved road: It’s comfortable to walk,﻿ but no flowers
+          grow on it.” ― Vincent van Gogh
+        </h2>
+      </div>
     </div>
   );
 }

ae95d287bfb76b43041386c7600d3d8c2980c071
Nicole Barranca <nikkibarranca@gmail.com>
2022-07-11 21:32:17 -0400

    hero mobile responsiveness

--- src/components/Homepage/index.js
+++ src/components/Homepage/index.js
@@ -1,15 +1,17 @@
 import React from "react";
-import placeholder from "../../img/placeholder.jpg"
+import placeholder from "../../img/placeholder.jpg";
 
-function Homepage(){
-    return (
-        <div id="parent">
-        <div id="hero"><img src={placeholder} alt={"abstract art"}  /></div>
-                
-        <h1 className="heroTitle">Multimedia realism meats abstract</h1>
-        <button className="btn">Veiw Gallery</button>
-        </div>
-    )
+function Homepage() {
+  return (
+    <div id="parent">
+      <div id="hero">
+        <img src={placeholder} alt={"abstract art"} />
+      </div>
+
+      <h1 className="heroTitle">Multimedia realism meats abstract</h1>
+      <button className="btn">Veiw Gallery</button>
+    </div>
+  );
 }
 
 export default Homepage;

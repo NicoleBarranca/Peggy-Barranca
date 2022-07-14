7c0b5579920af19d902307305b1e4aabd3dbb1d2
Nicole Barranca <nikkibarranca@gmail.com>
2022-07-14 14:41:41 -0400

    social media icons and list style

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,29 +1,31 @@
 import React from "react";
+import IG from "../../img/IG.png";
+import FB from "../../img/FB.png";
 
 function Footer() {
   return (
     <div className="footer">
-      <div className="fMenuItems">
+      <div>
         <h2>Learn More</h2>
-        <ul>
+        <ul className="fMenuItems">
           <li>About</li>
           <li>Gallery</li>
           <li>Blog</li>
         </ul>
       </div>
-      <div className="fMenuItems">
+      <div>
         <h2>Contact</h2>
-        <ul>
+        <ul className="fMenuItems">
           <li>Maggie Magee</li>
           <li>Email@email.com</li>
           <li>732732732732</li>
         </ul>
       </div>
-      <div className="fMenuItems">
+      <div>
         <h2>Follow Maggie</h2>
         <ul>
-          <img src="" alt="Instagram Logo" />
-          <img src="" alt="Facebook Logo" />
+          <img src={IG} alt="Instagram Logo" />
+          <img src={FB} alt="Facebook Logo" />
         </ul>
       </div>
     </div>

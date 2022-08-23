f6be4dd160e7160a7910a3215eafd92b5d995cf5
Nicole Barranca <nikkibarranca@gmail.com>
2022-08-23 17:04:24 -0400

    about page style

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,28 +3,43 @@ import avatar from "../img/avatar.png";
 function About() {
   return (
     <div className="about">
-      <h1>Maggie Maggie</h1>
-      <img src={avatar} alt={"logo"} id="logo" style={{ width: 200 }} />
-      <p>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aperiam
-        voluptas maxime consequatur magnam, itaque eligendi architecto
-        aspernatur quibusdam hic similique aut nihil temporibus, repudiandae
-        culpa dignissimos doloremque, enim incidunt. Lorem ipsum dolor, sit amet
-        consectetur adipisicing elit. Aperiam voluptas maxime consequatur
-        magnam, itaque eligendi architecto aspernatur quibusdam hic similique
-        aut nihil temporibus, repudiandae culpa dignissimos doloremque, enim
-        incidunt.
-      </p>
-      <p>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aperiam
-        voluptas maxime consequatur magnam, itaque eligendi architecto
-        aspernatur quibusdam hic similique aut nihil temporibus, repudiandae
-        culpa dignissimos doloremque, enim incidunt. Lorem ipsum dolor, sit amet
-        consectetur adipisicing elit. Aperiam voluptas maxime consequatur
-        magnam, itaque eligendi architecto aspernatur quibusdam hic similique
-        aut nihil temporibus, repudiandae culpa dignissimos doloremque, enim
-        incidunt.
-      </p>
+      <h1>Who is Peggy Barranca? </h1>
+      <div className="aboutContent">
+        <div>
+          {" "}
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aperiam
+            voluptas maxime consequatur magnam, itaque eligendi architecto
+            aspernatur quibusdam hic similique aut nihil temporibus, repudiandae
+            culpa dignissimos doloremque, enim incidunt. Lorem ipsum dolor, sit
+            amet consectetur adipisicing elit. Aperiam voluptas maxime
+            consequatur magnam, itaque eligendi architecto aspernatur quibusdam
+            hic similique aut nihil temporibus, repudiandae culpa dignissimos
+            doloremque, enim incidunt.
+          </p>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aperiam
+            voluptas maxime consequatur magnam, itaque eligendi architecto
+            aspernatur quibusdam hic similique aut nihil temporibus, repudiandae
+            culpa dignissimos doloremque, enim incidunt. Lorem ipsum dolor, sit
+            amet consectetur adipisicing elit. Aperiam voluptas maxime
+            consequatur magnam, itaque eligendi architecto aspernatur quibusdam
+            hic similique aut nihil temporibus, repudiandae culpa dignissimos
+            doloremque, enim incidunt.
+          </p>
+        </div>
+        <img
+          src={avatar}
+          alt={"logo"}
+          id="logo"
+          style={{
+            maxWidth: 400,
+            minWidth: 400,
+            maxHeight: 400,
+            minHeight: 400,
+          }}
+        />
+      </div>
     </div>
   );
 }

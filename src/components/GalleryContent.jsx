bc1671653edebda14874ccabc711a9f1ce3cb758
Nicole Barranca <nikkibarranca@gmail.com>
2022-09-01 12:21:19 -0400

    modal being displayed with react icons

--- src/components/GalleryContent.jsx
+++ src/components/GalleryContent.jsx
@@ -1,18 +1,44 @@
-// import { useState } from "react";
+import { useState } from "react";
 import "./gallery.css";
+import {
+  BsFillArrowLeftCircleFill,
+  BsFillArrowRightCircleFill,
+} from "react-icons/bs";
+import { AiFillCloseCircle } from "react-icons/ai";
 
 const GalleryContent = ({ galleryImages }) => {
-  // const [slideNumber, setSlideNumber] = useState(0);
-  // const [openModal, setOpenModal] = useState(false);
+  const [slideNumber, setSlideNumber] = useState(0);
+  const [openModal, setOpenModal] = useState(false);
+
+  // Function to open gallery image modal
+  const handleOpenModal = (index) => {
+    setSlideNumber(index);
+    setOpenModal(true);
+  };
 
   return (
     <div className="gallery">
+      {/* If modal is opened, show this div */}
+      {openModal && (
+        <div className="sliderWrap">
+          <AiFillCloseCircle className="btnClose" />
+          <BsFillArrowLeftCircleFill className="btnPrev" />
+          <BsFillArrowRightCircleFill className="btnNext" />
+          <div className="fullScreenImage">
+            <img src={galleryImages[slideNumber].photo} alt="" />
+          </div>
+        </div>
+      )}
       <h1>The Gallery</h1>
       <div className="galleryWrap">
         {galleryImages &&
           galleryImages.map((slide, index) => {
             return (
-              <div className="single" key={index}>
+              <div
+                className="single"
+                key={index}
+                onClick={() => handleOpenModal(index)}
+              >
                 <img
                   src={slide.photo}
                   alt=""
